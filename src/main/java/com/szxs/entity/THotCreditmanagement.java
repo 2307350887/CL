054cc8c841d3b61784a68a774d41e0271e062aec
2751720193 <2751720193@qq.com>
2018-11-13 12:01:36 +0800

    实体类

---
+++ src/main/java/com/szxs/entity/THotCreditmanagement.java
@@ -0,0 +1,36 @@
+package com.szxs.entity;
+
+
+public class THotCreditmanagement {
+
+  private String uuid;
+  private String userid;
+  private int creditmanagementnum;
+
+
+  public String getUuid() {
+    return uuid;
+  }
+
+  public void setUuid(String uuid) {
+    this.uuid = uuid;
+  }
+
+
+  public String getUserid() {
+    return userid;
+  }
+
+  public void setUserid(String userid) {
+    this.userid = userid;
+  }
+
+
+  public int getCreditmanagementnum() {
+    return creditmanagementnum;
+  }
+
+  public void setCreditmanagementnum(int creditmanagementnum) {
+    this.creditmanagementnum = creditmanagementnum;
+  }
+}
