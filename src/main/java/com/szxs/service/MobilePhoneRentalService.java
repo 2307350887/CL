54bbc6a70a1dd6e17d5f3e28d037dac9d97a8331
2751720193 <2751720193@qq.com>
2018-11-13 12:23:00 +0800

    service接口

--- src/main/java/com/szxs/service/MobilePhoneRentalService.java
+++ src/main/java/com/szxs/service/MobilePhoneRentalService.java
@@ -2,19 +2,30 @@ package com.szxs.service;
 
 import com.szxs.entity.Brand;
 import com.szxs.entity.MobilePhoneRental;
+import com.szxs.entity.THotCreditmanagement;
+import com.szxs.entity.THotDevice;
 
 import java.util.List;
 
 public interface MobilePhoneRentalService {
     /**
-     * 查询 手机租赁
-     * @param id 手机租赁 id
+     * 查询 租机机型
      * @return
      */
-    public List<MobilePhoneRental> selectMobilePhoneRental(String id);
+    public List<THotDevice> selectTHotDeviceList();
     /**
-     * 查询 手机品牌
+     * 查询 租机品牌
      * @return
      */
     public List<Brand> selectBrandList();
+    /**
+     * 查询 用户信用值
+     * @return
+     */
+    public List<THotCreditmanagement> selectTHotCreditmanagementList();
+    /**
+     * 查询 手机热门租机
+     * @return
+     */
+    public List<MobilePhoneRental> selectMobilePhoneRental();
 }
